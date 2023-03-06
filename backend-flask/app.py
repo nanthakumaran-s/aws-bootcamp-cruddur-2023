b9bb5df6f52d2ec88071dd1afa12628b0825712c
Nanthakumaran S <nanthankumaransenthil@gmail.com>
2023-03-06 09:07:02 +0000

    feat: added xray and instrumented traces

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -36,7 +36,17 @@ provider.add_span_processor(simple_processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
+# X-Ray
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+
 app = Flask(__name__)
+
+# X-Ray instrumentation
+XRayMiddleware(app, xray_recorder)
 
 # Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
